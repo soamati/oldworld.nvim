5cf3e301081ad1ec5a900d604348a957ea9d78cf
Matias Ruiz <matiruizsh@gmail.com>
2024-07-10 00:46:59 -0300

    add wezterm extra

---
+++ extras/wezterm/oldworld.lua
@@ -0,0 +1,33 @@
+return {
+	foreground = "#c9c7cd",
+	background = "#161617",
+
+	cursor_bg = "#c9c7cd",
+	cursor_fg = "#161617",
+	cursor_border = "#c9c7cd",
+
+	selection_fg = "#c9c7cd",
+	selection_bg = "#92a2d5",
+
+	ansi = {
+		"#27272a",
+		"#ea83a5",
+		"#90b99f",
+		"#e6b99d",
+		"#92a2d5",
+		"#aca1cf",
+		"#85b5ba",
+		"#c9c7cd",
+	},
+
+	brights = {
+		"#6c6874",
+		"#f591b2",
+		"#9dc6ac",
+		"#f0c5a9",
+		"#a6b6e9",
+		"#b9aeda",
+		"#99c9ce",
+		"#efeae7",
+	},
+}
