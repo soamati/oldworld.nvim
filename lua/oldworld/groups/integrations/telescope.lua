480029950ae4963a96e64000619bffe4b51bf2d2
Matias Ruiz <matiruizsh@gmail.com>
2024-07-10 01:28:45 -0300

    adjust telescope title fg

--- lua/oldworld/groups/integrations/telescope.lua
+++ lua/oldworld/groups/integrations/telescope.lua
@@ -3,8 +3,8 @@ local p = require("oldworld.palette")
 return {
     TelescopeBorder = { fg = p.gray3, bg = p.bg },
     TelescopeNormal = { fg = p.fg, bg = p.bg },
-    TelescopePreviewTitle = { fg = p.bg, bg = p.bg },
-    TelescopeResultsTitle = { fg = p.bg, bg = p.bg },
+    TelescopePreviewTitle = { fg = p.fg, bg = p.bg },
+    TelescopeResultsTitle = { fg = p.fg, bg = p.bg },
     TelescopePromptTitle = { fg = p.fg, bg = p.bg, italic = true },
     TelescopePromptBorder = { fg = p.gray3, bg = p.bg },
     TelescopePromptNormal = { fg = p.fg, bg = p.bg },
