e15803b15e6387559ec904e996da6e04bebece0b
Matias Ruiz <matiruizsh@gmail.com>
2026-07-04 23:59:16 -0300

    update bg

--- lua/oldworld/palette.lua
+++ lua/oldworld/palette.lua
@@ -1,11 +1,11 @@
 local palette = {
-	bg = "#0a0a0a",
+	bg = "#000000",
 	fg = "#c9c7cd",
 	subtext1 = "#b4b1ba",
 	subtext2 = "#9f9ca6",
 	subtext3 = "#8b8693",
 	subtext4 = "#6c6874",
-	bg_dark = "#0a0a0a",
+	bg_dark = "#000000",
 	black = "#27272a",
 	red = "#f49a9a",
 	green = "#9ad6a4",
